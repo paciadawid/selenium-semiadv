1965c57c8825c7a77a99c3ec42bf9b92fcd8bff7
paciadawid <paciadawid@gmail.com>
2022-12-22 09:42:12 +0100

    filter PoP

--- pages/base.py
+++ pages/base.py
@@ -1,8 +1,29 @@
 from selenium import webdriver
+from selenium.common import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC  # noqa
 
 
 class BasePage:
 
+    product_tab_selector = (By.CSS_SELECTOR, "[href='/products']")
+    search_product_selector = (By.ID, "search_product")
+    iframe1_ads_selector = (By.XPATH, "/html/ins//iframe[contains(@name, 'swift')]")
+    iframe2_ads_name = "ad_iframe"
+    dismiss_button_selector = (By.ID, "dismiss-button")
+
     def __init__(self, driver: webdriver.Chrome):
         self.driver = driver
 
+    def check_ads(self):
+        self.driver.find_element(*self.product_tab_selector).click()
+        try:
+            WebDriverWait(self.driver, 1).until(EC.visibility_of_element_located(self.search_product_selector)).click()
+        except TimeoutException:
+            iframe = self.driver.find_element(*self.iframe1_ads_selector)
+            self.driver.switch_to.frame(iframe)
+            self.driver.switch_to.frame(self.iframe2_ads_name)
+            self.driver.find_element(*self.dismiss_button_selector).click()
+            self.driver.switch_to.parent_frame()
+            WebDriverWait(self.driver, 3).until(EC.visibility_of_element_located(self.search_product_selector)).click()
--- pages/products.py
+++ pages/products.py
@@ -10,6 +10,9 @@ class ProductsPage(BasePage):
     search_field_selector = (By.ID, "search_product")
     submit_search_selector = (By.ID, "submit_search")
     single_product_selector = (By.CLASS_NAME, "single-products")
+    category_button_selector = (By.CSS_SELECTOR, ".category-products .fa-plus")
+    subcategory_button_selector = (By.XPATH, "//div[@class='panel-collapse in']//a")
+    brand_selector = (By.CSS_SELECTOR, ".brands-name span")
 
     def search_product(self, product):
         self.driver.find_element(*self.products_tab_selector).click()
@@ -21,3 +24,13 @@ class ProductsPage(BasePage):
         elements = WebDriverWait(self.driver, 10).until(
             EC.visibility_of_all_elements_located(self.single_product_selector))
         return elements
+
+    def select_category(self):
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.category_button_selector)).click()
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.subcategory_button_selector)).click()
+
+    def select_brand(self):
+        brand = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.brand_selector))
+        items_amount = int(brand.text[1:-1])
+        brand.click()
+        return items_amount
